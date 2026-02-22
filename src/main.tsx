35acb24b5d85bc0789d731dc281e83f8d80fe265
ion.osipciuc <ion.osipciuc@pynbooking.com>
2026-02-22 12:00:39 +0200

    test deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('test-deploy', import.meta.env);
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        {/*asdasd*/}
         <App />
     </StrictMode>
 );
